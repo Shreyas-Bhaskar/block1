207cf2bb91541e977547611de3eb11c8e9f36878
Shreyas Bhaskar <shreyasbhaskar@Shreyass-MacBook-Pro.local>
2020-05-11 16:32:14 +0530

    Updates

---
+++ precache-manifest.8245f6ad689dbe58352b177b742a897f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f5346e24c67db4f688",
+    "url": "/block1/static/css/main.eb3ec373.chunk.css"
+  },
+  {
+    "revision": "c8f5346e24c67db4f688",
+    "url": "/block1/static/js/main.c8f5346e.chunk.js"
+  },
+  {
+    "revision": "fc1ef83e053969006852",
+    "url": "/block1/static/css/1.828114f0.chunk.css"
+  },
+  {
+    "revision": "fc1ef83e053969006852",
+    "url": "/block1/static/js/1.fc1ef83e.chunk.js"
+  },
+  {
+    "revision": "e24d8061bdb5cabbcf02",
+    "url": "/block1/static/js/runtime~main.e24d8061.js"
+  },
+  {
+    "revision": "20b1c5ecd5d5e9072ef897a847013ce4",
+    "url": "/block1/index.html"
+  }
+];
